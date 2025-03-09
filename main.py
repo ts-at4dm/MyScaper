101d976443e08d761007845ef7fb9c2d2bfdb39d
ts-at4dm <terran.stone@maine.edu>
2025-03-09 01:39:46 -0500

    added write function

--- main.py
+++ main.py
@@ -3,11 +3,16 @@ from bs4 import BeautifulSoup
 
 def main():
     
-    url_input = input("Enter URL here: ")
-    url = url_input
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'html.parser')
-    print(soup)
+    url_input = input("Enter URL here: ") # Takes input from the user
+    response = requests.get(url_input) # requests the data from the given URL
+    
+    soup = BeautifulSoup(response.text, 'html.parser') # Takes raw data, parses the raw data into a 'BeautifulSoup' Object
+    
+    print(soup) # Prints the data to the console for testing
+    
+    # Writes the data to an output file
+    with open("output.txt", "w", encoding="utf-8") as file:
+        file.write(str(soup))
     
 if __name__ == "__main__": 
     main()
