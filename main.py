2cd8f65342172fe4d8855f331b8e4a44c437914d
ts-at4dm <terran.stone@maine.edu>
2025-03-09 13:11:40 -0400

    Swapped out frameworks
    Swapped BeautifulSoup for selenium and redeveloped the code to scrape for specific data. Attempting to create a bot that tells me when an item is in sstock once it becomes in stock

--- main.py
+++ main.py
@@ -1,18 +1,46 @@
-import requests
-from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+
 
 def main():
     
-    url_input = input("Enter URL here: ") # Takes input from the user
-    response = requests.get(url_input) # requests the data from the given URL
+    url_input = input("Enter Url: ")
+    options = Options()
+    options.add_argument("--headless")
+    options.add_argument("--disable-blink-features=AutomationControlled")
+    options.add_argument("start--maximized")
+    options.add_argument("disable-infobars")
+    options.add_argument("--disable-extentions")
     
-    soup = BeautifulSoup(response.text, 'html.parser') # Takes raw data, parses the raw data into a 'BeautifulSoup' Object
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
     
-    print(soup) # Prints the data to the console for testing
+    driver.get(url_input)
+    time.sleep(5)
     
-    # Writes the data to an output file
+    try:
+        title = driver.find_element(By.TAG_NAME, "h1").text
+    except:
+        title = "Title not found"
+        
+    try: 
+        price = driver.find_element(By.CLASS_NAME, "price-current").text
+    except:
+        price = "Price not found"
+        
+    try:
+        stock = driver.find_element(By.CLASS_NAME, "flags-body has-icon-left fa-exclamation-triangle").text
+    except:
+        stock = "In Stock"
+
     with open("output.txt", "w", encoding="utf-8") as file:
-        file.write(str(soup))
+        file.write(f"Title: {title}\nPrice: {price}\nStock: {stock}")
+    
+    driver.quit()
+
     
 if __name__ == "__main__": 
     main()
