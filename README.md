f50db1b6ec0cebb1828ff5ae77b416757ed0e0f8
ts-at4dm <terran.stone@maine.edu>
2025-03-09 01:24:08 -0500

    added basic code structure

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 <div align="center">
   <h1>My Web Scraper</h1>
 </div>
+<div align="left">
+  <p>This app is in the initial stages of development.
+  <br>
+  Please check back at a later date.
+  <p>
+</div>
+
--- main.py
+++ main.py
@@ -1,5 +1,13 @@
+import requests
+from bs4 import BeautifulSoup
+
 def main():
-    pass
+    
+    url_input = input("Enter URL here: ")
+    url = url_input
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    print(soup)
     
 if __name__ == "__main__": 
     main()
